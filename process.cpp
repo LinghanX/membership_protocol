0a7cb8f983cfdbc82cc02b7667b22c88c4f1c65f
LinghanX <linghan@me.com>
2018-10-28 19:40:07 -0400

    debug

--- process.cpp
+++ process.cpp
@@ -423,6 +423,7 @@ void Process::start_member() {
             logger -> error("recv error");
         } else {
             logger -> info("receved message");
+            logger -> info("is it equal to view msg: {}", numbytes == sizeof(new_view_msg));
             // got message from a client
             msg_type type = check_msg_type(buf, numbytes);
             switch (type) {
